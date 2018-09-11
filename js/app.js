cc1b82eced307944dae62c7e17760731620ed0c4
Stephen Snyder <ssnyder555@live.com>
2018-09-11 15:07:41 -0500

    this is my lab on loops

---
+++ js/app.js
@@ -0,0 +1,41 @@
+console.log("Making your way in the world today takes everything you've got.");
+//////console.log("Where everybody knows your name,");
+//console.log("and they're always glad you came.");
+//console.log("You wanna be where you can see,");
+//console.log("our troubles are all the same");
+//console.log("You wanna be where" "everybody knows");
+//console.log("Your name.");
+
+
+
+
+//What is the difference between:
+
+//the assignment operator =
+
+
+//and the equality comparison operator ==
+
+//and the strict equality comparison operator ===
+
+// Part 1
+//let num = 1;
+ //while(num <= 100){
+   //console.log('Ginger chocolate honey patties')
+  // num ++
+// }
+
+//part2
+//Write another while loop that counts from 0 to 1000 and will log in the console the current loop number.
+
+//let num = 0;
+//while(num => 1000){
+  //console.log(num);
+  //num ++
+//}
+
+let num = 0;
+while(num => 1000){
+  console.log(`Current loop number is:` + num);
+  num ++
+}
